b13eee9958b51112d6315b8893fa7d2d76ffb79e
Torben Binder <t.binder@steinberg.de>
2018-01-26 21:12:24 +0100

    Add basic jump functionality

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,21 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
 	public float movementSpeed;
+	
+	public Vector3 jump;
+	public float jumpForce = 2.0f;
+     
+	public bool isGrounded;
+	Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody>();
+		jump = new Vector3(0.0f, 2.0f, 0.0f);
+	}
+	
+	void OnCollisionStay()
+	{
+		isGrounded = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		var x = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
-//		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
-//		transform.Rotate(0, x, 0);
 		transform.Translate(x, 0, 0);
+		
+		if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
+			rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+			isGrounded = false;
+		}
 	}
 }
