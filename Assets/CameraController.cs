49263c1baa736642e3bd5206371d124eda81e896
Max Bebök <m.beboek@gmx.de>
2018-01-26 23:05:20 +0100

    added smooth camera that follows the player, smooth factor can be changed in unity

---
+++ Assets/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Camera playerCamera;
+	public Rigidbody playerBody;
+	
+	// Camera settings
+	public float camSmoothVal = 0.25f;
+	
+	private Vector3 lastCamPos;
+	
+	
+	// Use this for initialization
+	void Start () {
+		lastCamPos = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		// get player pos. limited to X/Y
+		var newCamPos = playerBody.position;
+		newCamPos.z = lastCamPos.z;
+		
+		// follow only a fraction of the distance
+		transform.position += (newCamPos - lastCamPos) * camSmoothVal;
+		
+		lastCamPos = transform.position;
+	}
+}
