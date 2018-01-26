13f941fa09cc3fd969829bb01756f2a87301fa24
Torben Binder <t.binder@steinberg.de>
2018-01-27 00:51:09 +0100

    Add PlayerSpawn

---
+++ Assets/SpawnController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SpawnController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SpawnController spawnController = FindObjectOfType<SpawnController>();
+		
+//		Create
+		
+		transform.position = spawnController.transform.position;
+	}
+}
