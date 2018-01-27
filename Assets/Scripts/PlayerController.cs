869d6d60c8a55e7d29bbd1688d7c5b95cb6a72a5
Torben Binder <t.binder@steinberg.de>
2018-01-27 01:32:06 +0100

    Add spawn script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,50 +1,71 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-	public float movementSpeed;
-	
-	public Vector3 jump;
-	public float jumpForce = 2.0f;
-     
-	public bool isGrounded;
-	Rigidbody rb;
+    public float movementSpeed;
 
-	// Use this for initialization
-	void Start () {
-		rb = GetComponent<Rigidbody>();
-		jump = new Vector3(0.0f, 2.0f, 0.0f);
-	}
-	
-	void OnCollisionStay()
-	{
-		isGrounded = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+
+    public bool isGrounded;
+
+    Rigidbody rb;
+
+    public float deathBarrierY = -40.0f;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+
+        respawn();
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    void Update()
+    {
+        if (transform.position.y < deathBarrierY)
+        {
+            respawn();
+        }
+
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
-         transform.Translate(x, 0, 0);
-        if (Input.GetKeyDown(KeyCode.LeftShift)) {
+        transform.Translate(x, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
             movementSpeed = movementSpeed / 2;
         }
-        if (Input.GetKeyUp(KeyCode.LeftShift)) {
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
             movementSpeed = movementSpeed * 2;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded){
-			rb.AddForce(jump * jumpForce, ForceMode.Impulse);
-			isGrounded = false;
-		}
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Item")) {
+        if (other.gameObject.CompareTag("Item"))
+        {
             other.gameObject.SetActive(false);
         }
+    }
+
+    private void respawn()
+    {
+        var spawn = FindObjectOfType<SpawnController>();
+        transform.position = spawn.transform.position;
     }
 }
