b924dc9eba04dd739dfd30173acbf724e9d1edf6
AWlochowitz <a.wlochowitz@steinberg.de>
2018-01-27 16:55:28 +0100

    add stone prefab

---
+++ Assets/Scripts/AI/StoneController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneController : MonoBehaviour {
+
+    public NoiseController noiseArea;
+
+    public float noise = 2.0f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        noiseArea.applyNoise(collision.impulse.magnitude * noise);
+    }
+}
