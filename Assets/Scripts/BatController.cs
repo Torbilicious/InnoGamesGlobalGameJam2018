034d5a4897e05113c97b539c03053bd61d25791d
Marcel Bulla <gmx@marcel-bulla.de>
2018-01-28 12:32:05 +0100

    Add bats

---
+++ Assets/Scripts/BatController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatController : MonoBehaviour
+{
+	private float x = 0.5f, y = 0.5f;
+	
+	// Use this for initialization
+	void Start () {
+		Random.InitState(83495903);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if (Random.Range(0, 100) <= 1)
+		{
+			x *= -1;
+		}
+		if (Random.Range(0, 100) <= 1)
+		{
+			y *= -1;
+		}
+		
+		transform.Translate(x * Time.fixedDeltaTime, y * Time.fixedDeltaTime, 0);
+	}
+}
