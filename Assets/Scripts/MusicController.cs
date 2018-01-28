6c0e326786bdac9601c5bbe4b076b54b7954315c
Marcel Bulla <gmx@marcel-bulla.de>
2018-01-28 13:38:42 +0100

    Add alarm music

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.AI;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var alarm = GetComponents<AudioSource>()[1];
+		alarm.Play();
+		alarm.Pause();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		var enemyControllers = FindObjectsOfType<EnemyController>();
+
+		var isAlarm = enemyControllers.FirstOrDefault(e => e.isFollowing) != null;
+
+		var alarm = GetComponents<AudioSource>()[1];
+		var main =  GetComponents<AudioSource>()[0];
+		
+		if (isAlarm && !alarm.isPlaying)
+		{
+			main.Pause();
+			alarm.UnPause();
+		}
+		else if(!isAlarm && alarm.isPlaying)
+		{
+			main.UnPause();
+			alarm.Pause();
+		}
+	}
+}
