7fc630936ae5caf7167446b1e520fc17740952e3
AWlochowitz <a.wlochowitz@steinberg.de>
2018-01-28 14:16:51 +0100

    add main menu

---
+++ Assets/MenuController.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class MenuController : MonoBehaviour {
+
+    public Sprite StartGame;
+    public Sprite EndGame;
+    public Sprite Credits;
+    public Sprite Controls;
+
+    public float waitTime = 0.5f;
+    private float currentWaitTime = 0;
+
+    private int position = 0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentWaitTime -= Time.fixedDeltaTime;
+
+        userInput();
+
+
+        updatePosition();
+        if (currentWaitTime < 0.0f)
+        {
+            setImage();        }
+        
+    }
+
+    private void updatePosition()
+    {
+        var isLeft = CrossPlatformInputManager.GetAxis("Horizontal") > 0;
+        var isRight = CrossPlatformInputManager.GetAxis("Horizontal") < 0;
+        if (isLeft)
+        {
+            position++;
+
+        }
+        if (isRight)
+        {
+            position--;
+        }
+        if (position < 0)
+        {
+            position = 3;
+        }
+        if (position > 3)
+        {
+            position = 0;
+        }
+    }
+
+    private void userInput()
+    {
+
+        if (Input.GetButton("Jump") || CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            switch (position)
+            {
+                case 0:
+                    LoadScene();
+                    break;
+                case 1:
+                    Application.Quit();
+                    break;
+                case 2:
+              
+                    break;
+                case 3:
+
+                    break;
+                default:
+                    LoadScene();
+                    break;
+            }
+        }
+    }
+
+    private void setImage()
+    {
+        var images = gameObject.GetComponentsInChildren<Image>();
+
+        foreach (Image image in images)
+        {
+
+            Sprite currentImage;
+
+            switch (position)
+            {
+                case 0:
+                    currentImage = StartGame;
+                    break;
+                case 1:
+                    currentImage = EndGame;
+                    break;
+                case 2:
+                    currentImage = Credits;
+                    break;
+                case 3:
+                    currentImage = Controls;
+                    break;
+                default:
+                    currentImage = StartGame;
+                    break;
+            }
+            // var currentImage = isAlarm ? alarm : undetected;
+            image.sprite = currentImage;
+            currentWaitTime = waitTime;
+        }
+    }
+
+    public void LoadScene()
+    {
+        Debug.Log("Load Scene 1");
+        SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+    }
+}
---
+++ Assets/Scripts/GUI/MainMenu/MainMenuController.cs
@@ -0,0 +1,83 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using System;
+//using UnityEngine.UI;
+//using UnityEngine.SceneManagement;
+//using UnityStandardAssets.CrossPlatformInput;
+
+//public class MainMenuController : MonoBehaviour {
+
+//    Button btnStart;
+//    Button btnEnd;
+//    Button btnCredits;
+
+//    // Use this for initialization
+//    void Start () {
+//        btnStart = GameObject.Find("Start Game").GetComponent<UnityEngine.UI.Button>();
+//        btnEnd = GameObject.Find("End Game").GetComponent<UnityEngine.UI.Button>();
+//        btnCredits = GameObject.Find("Credits").GetComponent<UnityEngine.UI.Button>();
+
+//        btnStart.onClick.AddListener(LoadScene);
+//    }
+	
+//	// Update is called once per frame
+//	void Update () {
+
+//        //Debug.Log(btnStart);
+//        if(btnStart.gameObject.activeSelf)
+//        {
+//            btnEnd.gameObject.SetActive(false);
+//            btnCredits.gameObject.SetActive(false);
+//        }
+       
+
+//        if (Input.GetButton("Jump") || CrossPlatformInputManager.GetButtonDown("Jump"))
+//        {
+//            //Debug.Log("jump");
+//            LoadScene();
+//        }
+
+//        if(Math.Abs(CrossPlatformInputManager.GetAxis("Horizontal")) > 0)
+//        {
+//            if(CrossPlatformInputManager.GetAxis("Horizontal") < 0 && btnStart.gameObject.activeSelf)
+//            {
+//                // Left
+//                btnStart.gameObject.SetActive(false);
+//                btnCredits.gameObject.SetActive(false);
+//                btnEnd.gameObject.SetActive(true);              
+
+//            }
+//            if (CrossPlatformInputManager.GetAxis("Horizontal") > 0 && btnStart.gameObject.activeSelf)
+//            {
+//                btnStart.gameObject.SetActive(false);
+//                btnCredits.gameObject.SetActive(true);
+//                btnEnd.gameObject.SetActive(false);
+//                // Right
+//            }
+
+//            if (CrossPlatformInputManager.GetAxis("Horizontal") > 0 && btnEnd.gameObject.activeSelf)
+//            {
+//                btnEnd.gameObject.SetActive(false);
+//                btnStart.gameObject.SetActive(true);
+//                btnCredits.gameObject.SetActive(false);
+//                // Right
+//            }
+
+//            if (CrossPlatformInputManager.GetAxis("Horizontal") < 0 && btnCredits.gameObject.activeSelf)
+//            {
+//                btnCredits.gameObject.SetActive(false);
+//                btnStart.gameObject.SetActive(true);
+//                btnEnd.gameObject.SetActive(false);
+//                // Right
+//            }
+//        }
+//    }
+
+//    public void LoadScene()
+//    {
+//        Debug.Log("Load Scene 1");
+//        SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+//    }
+
+//}
